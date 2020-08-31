18d5ff5b8de2a4d00e59e7391423ebb17cb9d7b0
vividswan <vividswan@naver.com>
2020-08-31 18:07:23 +0900

    Create 4_변수의 타입2.js

---
+++ 1부(1-50)/4_변수의 타입2.js
@@ -0,0 +1,12 @@
+var a = 1;
+console.log(typeof (a));
+// number
+var b = 2.22;
+console.log(typeof (b));
+// number
+var c = 'p';
+console.log(typeof (c));
+// string
+var d = [1, 2, 3];
+console.log(typeof (d));
+// object
