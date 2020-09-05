0bfc9405c9bbaf508de5f6fd5470f3a78f5127c6
vividswan <vividswan@naver.com>
2020-09-06 01:44:20 +0900

    Create 9_concat을 활용한 출력 방법.js

---
+++ 1부(1-50)/9_concat을 활용한 출력 방법.js
@@ -0,0 +1,11 @@
+var year = '2020';
+var month = '09';
+var day = '06';
+var hour = '01';
+var minute = '18';
+var second = '27';
+
+var result = year.concat('/', month, '/', day, ' ', hour, ':', minute, ':', second);
+
+console.log(result);
+// 2020/09/06 01:18:27 출력
